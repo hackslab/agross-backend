4b2252bf09d1f1b6530283ce7fcebf2cde4db67b
Abdulaziz <lil.gangsser@gmail.com>
2025-11-25 18:38:05 +0500

    feat: initialize NestJS application with core configurations, global validation, and Swagger API documentation.

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { json, urlencoded } from "express";
 
 const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(json({ limit: "100mb" }));
+  app.use(urlencoded({ extended: true, limit: "100mb" }));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   // Swagger Configuration
